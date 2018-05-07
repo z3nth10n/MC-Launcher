477e3b026aa263fb74395ec875f1c397932c3cc3
Ikillnukes <ikillnukes1@gmail.com>
2018-05-07 02:05:19 +0200

    Fix on bindings

--- bindings.sh
+++ bindings.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 rm -rf z3nth10n-PHP/
 cp -r /c/xampp/htdocs/z3nth10n-PHP/ z3nth10n-PHP/
-cd z3nth10n-PHP/
 read -p "Commit message: " commit_msg
-git add .
-git commit -m $commit_msg
-git push -u origin master
-cd ..
+git -C z3nth10n-PHP/ add .
+git -C z3nth10n-PHP/ commit -m $commit_msg
+git -C z3nth10n-PHP/ push -u origin master
