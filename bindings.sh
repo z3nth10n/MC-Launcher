79142d7ec8811fe317a256b26a47a91ec38c3744
Ikillnukes <ikillnukes1@gmail.com>
2018-05-07 02:09:14 +0200

    JSON Header

--- bindings.sh
+++ bindings.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm -rf z3nth10n-PHP/
 cp -r /c/xampp/htdocs/z3nth10n-PHP/ z3nth10n-PHP/
+fpath="$(pwd)/z3nth10n-PHP/"
 read -p "Commit message: " commit_msg
-git -C z3nth10n-PHP/ add .
-git -C z3nth10n-PHP/ commit -m $commit_msg
-git -C z3nth10n-PHP/ push -u origin master
+git -C "$fpath" add --all
+git -C "$fpath" commit -m "$commit_msg"
+git -C "$fpath" push -u origin master
