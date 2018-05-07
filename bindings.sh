bad46529a19f4faae2685be8b8c669d2326600b3
Ikillnukes <ikillnukes1@gmail.com>
2018-05-07 03:02:33 +0200

    Bindings updated

--- bindings.sh
+++ bindings.sh
@@ -1,13 +1,49 @@
 #!/bin/bash
-fpath="$(pwd)/z3nth10n-PHP/"
+fpath="z3nth10n-PHP"
 yes | cp -rf /c/xampp/htdocs/z3nth10n-PHP/ "$fpath"
-fpath+=".git"
-read -p "Commit message: " commit_msg
-git --git-dir="$fpath" add --all
-git --git-dir="$fpath" commit -m "$commit_msg"
-git --git-dir="$fpath" push -u origin master
 
-read -p "Commit message for this repo: " commit_msg
-git add --all
-git commit -m "$commit_msg"
-git push -u origin master
+function php_commit 
+{
+  read -p "Commit message: " commit_msg
+  if [[ ! -z `git -C "$fpath" diff HEAD .gitignore` ]]; then
+    git -C "$fpath" rm -rf --cached .
+  fi
+  git -C "$fpath" add --all
+  git -C "$fpath" commit -m "$commit_msg"
+  git -C "$fpath" push -u origin master
+}
+
+function main_commit 
+{
+  read -p "Commit message for this repo: " commit_msg
+  if [[ ! -z `git diff HEAD .gitignore` ]]; then
+    git -C "$fpath" rm -rf --cached .
+  fi
+  git add --all
+  git commit -m "$commit_msg"
+  git push -u origin master
+}
+
+echo ""
+echo "Please select an option:"
+echo "  0) Only PHP commit"
+echo "  1) Only main commit"
+echo "  2) Both commits"
+echo ""
+read -p "Select an option: " opt
+
+case $opt in
+"0")
+  php_commit
+  ;;
+"1")
+  main_commit
+  ;;
+"2")
+  php_commit
+  main_commit
+  ;;
+*)
+  echo "Unrecognized option, call 'git bind' again."
+  ;;
+esac
