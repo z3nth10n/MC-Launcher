863855a46666a69a67562065104a7f2fbcf95e2e
Ikillnukes <ikillnukes1@gmail.com>
2018-05-07 02:39:19 +0200

    Forgotten to remove cached files ^^'

--- z3nth10n-PHP/Settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbserver = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "z3nth10n-db";
-$privFile = "keys/private.key";
-$pubFile = "keys/public.key";
--- z3nth10n-PHP/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// *** Little guide *** //
-
-echo "Use api.php, WIP DOCS.";
