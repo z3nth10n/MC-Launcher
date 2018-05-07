818f4212390a9cbd3b86a8a61a04877e1c958801
Ikillnukes <ikillnukes1@gmail.com>
2018-05-07 02:03:51 +0200

    Working on things & bindings improved

--- bindings.sh
+++ bindings.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 rm -rf z3nth10n-PHP/
 cp -r /c/xampp/htdocs/z3nth10n-PHP/ z3nth10n-PHP/
+cd z3nth10n-PHP/
+read -p "Commit message: " commit_msg
+git add .
+git commit -m $commit_msg
+git push -u origin master
+cd ..
--- z3nth10n-PHP/api.php
+++ z3nth10n-PHP/api.php
@@ -8,6 +8,7 @@ include('actions.php');
 
 //actions_main($coreData);
 
+header('Content-Type: application/json');
 die(showJson($coreData));
 
 //Cerramos y mostramos errores
